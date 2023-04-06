011fcaab01018643a5b315053ef8852540944b6b
real-bombinho <37409530+real-bombinho@users.noreply.github.com>
2023-04-06 02:09:53 +0100

    Update AT24Cxx.h

--- AT24Cxx.h
+++ AT24Cxx.h
@@ -12,16 +12,16 @@
 
 class AT24Cxx {
 public:
-	AT24Cxx(uint8_t i2c_address);
-	AT24Cxx(uint8_t i2c_address, uint32_t eeprom_size);
-	uint8_t read(uint16_t address);
-	void write(uint16_t address, uint8_t value);
-//	void write(uint16_t address, uint8_t *ptr, uint8_t size);
-	void update(uint16_t address, uint8_t value);
+	AT24Cxx(const uint8_t i2c_address);
+	AT24Cxx(const uint8_t i2c_address, uint32_t eeprom_size);
+	uint8_t read(const uint16_t address);
+	void write(const uint16_t address, uint8_t value);
+//	void write(const uint16_t address, uint8_t *ptr, uint8_t size);
+	void update(const uint16_t address, uint8_t value);
 	uint32_t length(void);
 
-//	const int& operator[](uint16_t address) const;
-	int operator[](uint16_t address);  // Used to access
+//	const int& operator[](const uint16_t address) const;
+	int operator[](const uint16_t address);  // Used to access
 	
 	//AT24Cxx(const AT24Cxx& orig);
 	//virtual ~AT24Cxx();
